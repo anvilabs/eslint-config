39567f0b71b4dd0b775498f07cd0353f9040c9be
Ayan Yenbekbay <ayan.yenb@gmail.com>
2016-10-10 14:28:19 +0600

    feat(react-native): disable `no-use-before-define` rule

--- react-native.js
+++ react-native.js
@@ -6,6 +6,8 @@ module.exports = {
     'react-native',
   ],
   rules: {
+    'no-use-before-define': 0,
+
     'react-native/no-color-literals': 2,
     'react-native/no-inline-styles': 2,
     'react-native/no-unused-styles': 2,

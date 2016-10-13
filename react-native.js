0050431292cb2ee3dafcc45dc331436a7c32e956
Ayan Yenbekbay <ayan.yenb@gmail.com>
2016-10-13 15:48:00 +0600

    feat(react-native): disable some `eslint-plugin-fp` rules

--- react-native.js
+++ react-native.js
@@ -8,6 +8,19 @@ module.exports = {
   rules: {
     'no-use-before-define': 0,
 
+    'fp/no-class': 0,
+    'fp/no-mutation': [2, {
+      commonjs: true,
+      allowThis: true,
+      exceptions: [
+        { property: 'contextTypes' },
+        { property: 'defaultProps' },
+        { property: 'propTypes' },
+      ],
+    }],
+    'fp/no-this': 0,
+    'fp/no-unused-expression': 0,
+
     'react-native/no-color-literals': 2,
     'react-native/no-inline-styles': 2,
     'react-native/no-unused-styles': 2,

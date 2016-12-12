88ef7acc2631b07f7f6dcd9f1b7098328a089008
Ayan Yenbekbay <ayan.yenb@gmail.com>
2016-12-12 17:32:18 +0600

    feat(base): update eslint-plugin-promise and add its new rules

--- index.js
+++ index.js
@@ -67,8 +67,12 @@ module.exports = {
     'no-use-extend-native/no-use-extend-native': 2,
 
     'promise/always-return': 2,
+    'promise/avoid-new': 0,
     'promise/catch-or-return': 2,
+    'promise/no-callback-in-promise': 1,
     'promise/no-native': 0,
+    'promise/no-nesting': 1,
+    'promise/no-promise-in-callback': 1,
     'promise/no-return-wrap': 0, // https://github.com/xjamundx/eslint-plugin-promise/issues/34
     'promise/param-names': 2,
     'promise/prefer-await-to-callbacks': 2,

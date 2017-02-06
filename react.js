868d92ace064af34b7896eae08c8829a3e7c24e2
Ayan Yenbekbay <ayan.yenb@gmail.com>
2017-02-07 02:26:14 +0600

    feat: update to latest eslint and eslint-config-airbnb versions

--- react.js
+++ react.js
@@ -6,4 +6,12 @@ module.exports = {
     './react-base.js',
     'eslint-config-airbnb/rules/react-a11y',
   ],
+  rules: {
+    'jsx-a11y/accessible-emoji': 0,
+    'jsx-a11y/aria-activedescendant-has-tabindex': 0,
+    'jsx-a11y/iframe-has-title': 0,
+    'jsx-a11y/no-autofocus': 0,
+    'jsx-a11y/no-distracting-elements': 0,
+    'jsx-a11y/no-redundant-roles': 0,
+  },
 };

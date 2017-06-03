6d6a6758072c83cd3d5f92c0c510a8bc0df6272c
Ayan Yenbekbay <ayan.yenb@gmail.com>
2017-06-03 09:22:07 +0600

    refactor(flowtype): stop usage of plugin:flowtype/recommended config

--- flowtype.js
+++ flowtype.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['plugin:flowtype/recommended', 'prettier/flowtype'],
+  extends: ['prettier/flowtype'],
   plugins: ['flowtype'],
   parser: 'babel-eslint',
   settings: {
@@ -9,9 +9,12 @@ module.exports = {
   },
   rules: {
     // https://github.com/gajus/eslint-plugin-flowtype
+    'flowtype/boolean-style': [2, 'boolean'],
+    'flowtype/define-flow-type': 2,
     'flowtype/no-dupe-keys': 2,
     'flowtype/no-primitive-constructor-types': 2,
     'flowtype/no-types-missing-file-annotation': 2,
+    'flowtype/no-weak-types': 0,
     'flowtype/require-parameter-type': 0,
     'flowtype/require-return-type': 0,
     'flowtype/require-valid-file-annotation': [
@@ -24,6 +27,7 @@ module.exports = {
     'flowtype/require-variable-type': 0,
     'flowtype/sort-keys': 0,
     'flowtype/type-id-match': [2, '^([A-Z][a-z0-9]+)+Type$'],
+    'flowtype/use-flow-type': 2,
     'flowtype/valid-syntax': 0,
   },
 };

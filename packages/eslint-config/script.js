2a5d4d57205a85a39ed303b05b3482879b6e14f1
Ayan Yenbekbay <ayan.yenb@gmail.com>
2018-07-27 21:52:37 +0600

    fix(base): disable certain rules in script config

--- packages/eslint-config/script.js
+++ packages/eslint-config/script.js
@@ -2,8 +2,11 @@
 module.exports = {
   rules: {
     'global-require': 'off',
+    'no-console': 'off',
     // https://github.com/benmosher/eslint-plugin-import
     'import/no-extraneous-dependencies': 'off',
     'import/no-internal-modules': 'off',
+    // https://github.com/sindresorhus/eslint-plugin-unicorn
+    'unicorn/no-process-exit': 'off',
   },
 };

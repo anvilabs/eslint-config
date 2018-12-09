7226c6ef54a4dd0280f7e524959b8ab42b886c33
Ayan Yenbekbay <ayan.yenb@gmail.com>
2018-12-09 19:56:09 +0100

    fix(react): disable a buggy eslint-plugin-react-native rule

--- packages/eslint-config-react/native.js
+++ packages/eslint-config-react/native.js
@@ -41,7 +41,7 @@ module.exports = {
     // https://github.com/Intellicode/eslint-plugin-react-native
     'react-native/no-color-literals': 'error',
     'react-native/no-inline-styles': 'error',
-    'react-native/no-raw-text': 'error',
+    'react-native/no-raw-text': 'off', // buggy (https://github.com/Intellicode/eslint-plugin-react-native/issues/210)
     'react-native/no-unused-styles': 'error',
     'react-native/split-platform-components': 'error',
   },

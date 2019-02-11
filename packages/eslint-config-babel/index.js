d16960f552b2f4e867f6edcada25461def2c2b07
Ayan Yenbekbay <ayan.yenb@gmail.com>
2019-02-11 19:52:14 +0100

    feat(typescript): enable all relevant typescript rules

--- packages/eslint-config-babel/index.js
+++ packages/eslint-config-babel/index.js
@@ -2,11 +2,10 @@ module.exports = {
   plugins: ['babel'],
   parser: 'babel-eslint',
   rules: {
-    camelcase: 'off',
-    'no-unused-expressions': 'off', // disabled in favor of `babel/no-unused-expressions`
     // https://github.com/babel/eslint-plugin-babel
     'babel/array-bracket-spacing': 'off',
     'babel/arrow-parens': 'off',
+    camelcase: 'off',
     'babel/camelcase': ['error', {properties: 'never'}],
     'babel/flow-object-type': 'off',
     'babel/func-params-comma-dangle': 'off',
@@ -14,6 +13,7 @@ module.exports = {
     'babel/new-cap': 'off',
     'babel/no-await-in-loop': 'off',
     'babel/no-invalid-this': 'error',
+    'no-unused-expressions': 'off',
     'babel/no-unused-expressions': ['error', {allowShortCircuit: true}],
     'babel/object-curly-spacing': 'off',
     'babel/object-shorthand': 'off',
--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -12,13 +12,6 @@ module.exports = {
     'import/extensions': ['.js', '.ts'],
   },
   rules: {
-    camelcase: 'off',
-    'no-array-constructor': 'off',
-    'no-restricted-globals': 'off', // conflicts with typescript, @see https://github.com/eslint/typescript-eslint-parser/issues/350
-    'no-undef': 'off', // conflicts with typescript
-    'no-unused-vars': 'off', // disabled in favor of `typescript/no-unused-vars`
-    'no-use-before-define': 'off', // disabled in favor of `typescript/no-use-before-define`
-    'no-useless-constructor': 'off', // conflicts with typescript
     'spaced-comment': [
       'error',
       'always',
@@ -47,35 +40,59 @@ module.exports = {
     'import/no-named-as-default-member': 'off', // conflicts with typescript
     'import/no-named-as-default': 'off', // conflicts with typescript
     // https://github.com/nzakas/eslint-plugin-typescript
-    '@typescript-eslint/adjacent-overload-signatures': 'off', // TSLint is better at this
-    '@typescript-eslint/class-name-casing': 'off', // TSLint is better at this
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-member-accessibility': 'off', // TSLint is better at this
+    '@typescript-eslint/adjacent-overload-signatures': 'error',
+    '@typescript-eslint/array-type': 'error',
+    '@typescript-eslint/ban-types': 'error',
+    camelcase: 'off',
+    '@typescript-eslint/camelcase': 'error',
+    '@typescript-eslint/class-name-casing': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/explicit-member-accessibility': 'error',
+    indent: 'off',
     '@typescript-eslint/generic-type-naming': [
       'error',
       '^([TUKV]|T[A-Z][a-zA-Z]+)$',
     ],
-    '@typescript-eslint/interface-name-prefix': 'off', // TSLint is better at this
-    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/indent': 'error',
+    '@typescript-eslint/interface-name-prefix': 'error',
+    '@typescript-eslint/member-delimiter-style': 'error',
     '@typescript-eslint/member-naming': 'off',
-    '@typescript-eslint/member-ordering': 'off', // type annotations should come first
-    '@typescript-eslint/no-angle-bracket-type-assertion': 'off', // TSLint is better at this
+    '@typescript-eslint/member-ordering': 'error',
+    '@typescript-eslint/no-angle-bracket-type-assertion': 'error',
+    'no-array-constructor': 'off',
     '@typescript-eslint/no-array-constructor': 'error',
-    '@typescript-eslint/no-empty-interface': 'off', // TSLint is better at this
-    '@typescript-eslint/no-explicit-any': 'off', // TSLint is better at this
-    '@typescript-eslint/no-inferrable-types': 'off', // TSLint is better at this
-    '@typescript-eslint/no-namespace': 'off', // TSLint is better at this
-    '@typescript-eslint/no-non-null-assertion': 'off', // TSLint is better at this
-    '@typescript-eslint/no-parameter-properties': 'off', // TSLint is better at this
-    '@typescript-eslint/no-triple-slash-reference': 'off', // TSLint is better at this
-    '@typescript-eslint/no-type-alias': 'off', // TSLint is better at this
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-var-requires': 'off', // TSLint is better at this
-    '@typescript-eslint/no-use-before-define': [
+    '@typescript-eslint/no-empty-interface': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-extraneous-class': 'error',
+    '@typescript-eslint/no-for-in-array': 'error',
+    '@typescript-eslint/no-inferrable-types': 'error',
+    '@typescript-eslint/no-misused-new': 'error',
+    '@typescript-eslint/no-namespace': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    '@typescript-eslint/no-object-literal-type-assertion': 'error',
+    '@typescript-eslint/no-parameter-properties': 'error',
+    '@typescript-eslint/no-require-imports': 'off',
+    '@typescript-eslint/no-this-alias': 'error',
+    '@typescript-eslint/no-triple-slash-reference': 'error',
+    '@typescript-eslint/no-type-alias': [
       'error',
-      {functions: true, classes: true, variables: true},
+      {
+        allowAliases: 'always',
+        allowCallbacks: 'always',
+        allowLiterals: 'in-unions-and-intersections',
+        allowMappedTypes: 'always',
+      },
     ],
-    '@typescript-eslint/prefer-namespace-keyword': 'off', // TSLint is better at this
-    '@typescript-eslint/type-annotation-spacing': 'off', // not necessary with prettier
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-use-before-define': 'error',
+    '@typescript-eslint/no-useless-constructor': 'error',
+    '@typescript-eslint/no-var-requires': 'error',
+    '@typescript-eslint/prefer-interface': 'error',
+    '@typescript-eslint/prefer-namespace-keyword': 'error',
+    '@typescript-eslint/promise-function-async': 'off',
+    '@typescript-eslint/type-annotation-spacing': 'error',
+    '@typescript-eslint/restrict-plus-operands': 'error',
   },
 };
